6f58d8880dc48ac4c0883eb1e011f391678c18d9
[Andrew Core] <[adcore89@gmail.com]>
2021-06-27 08:39:30 -0400

    user controller started, get all route completed

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,22 @@
-const { User } = require('../models');
+const { User } = require("../models");
 
 const userController = {
+  // get all users
 
+  getAllUsers(req, res) {
+      User.find({})
+      .populate({ 
+          path: 'user',
+          select: '-__v'
+      })
+      .seletc('-__v')
+      .sort({_id: -1})
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+          console.log (err);
+          res.sendStatus(400):
+      });
+  },
 };
 
 module.exports = userController;
